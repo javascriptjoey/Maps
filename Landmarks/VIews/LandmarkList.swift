b529e6af5296fe411f5f00b46a328f2f17309ddb
Joey Essak <35553162+javascriptjoey@users.noreply.github.com>
2022-02-26 12:43:22 -0800

    Model set up and created, Landmark Row created and working

---
+++ Landmarks/VIews/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Joey Essak on 2/26/22.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
