669cef8f704b185ddef6dceba2d93ce5e9d83296
Joey <35553162+javascriptjoey@users.noreply.github.com>
2022-02-25 21:59:51 -0800

    Initial Commit

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Joey Essak on 2/25/22.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+            Image("turtlerock")
+                .clipShape(Circle())
+                .overlay {
+                    Circle().stroke(.white, lineWidth:   10)
+                }
+                .shadow(radius: 7)
+        }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,19 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Joey Essak on 2/25/22.
+//
+
+import SwiftUI
+
+//@main identifies app entry point
+@main
+
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
